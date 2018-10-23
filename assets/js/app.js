0e5804a87b35720dedaff00deba366c5f528cc1e
Brian Savage <brian.richard.savage@gmail.com>
2018-10-23 10:13:36 -0400

    completed step-102

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,22 @@
-
+// JavaScript Document
 
 window.onload = init();
 
 function init() {
 
-    window.addEventListener('scroll', function(e){
+    window.addEventListener('scroll', function (e) {
         var distanceY = window.pageYOffset || document.documentElement.scrollTop,
             shrinkOn = 300,
             header = document.querySelector("header");
         if (distanceY > shrinkOn) {
-            classie.add(header,"smaller");
+            classie.add(header, "smaller");
         } else {
-            if (classie.has(header,"smaller")) {
-                classie.remove(header,"smaller");
+            if (classie.has(header, "smaller")) {
+                classie.remove(header, "smaller");
             }
         }
     });
+
 
     $.ajax({
         method: 'GET',
@@ -23,26 +24,52 @@ function init() {
         dataType: 'json',
         success: function (data) {
 
-            console.log('all good');
-            console.log(data.menu.length);
-            console.log(data.menu);
+            var menu = menuBuilder(data.menu);
 
-            if (data.menu.length > 0) {
-                console.log('starting foreach');
-                data.menu.forEach(function (data) {
-                    console.log(data);
-                    console.log(data.MenuName);
-                    console.log(data.MenuLink);
-                    $('nav').append('<a href="' + data.MenuLink + '">' + data.MenuName + '</a>');
-                });
-                console.log('ending foreach');
-            }
+            $('nav').append(menu);
+
+            $("#loaderDiv").fadeOut("slow");
+
         },
         error: function () {
+
             console.log('all is not good');
+
         }
     });
 
+}
 
 
+function menuBuilder(obj) {
+
+    var theMenu = '';
+
+    if (obj.length > 0) {
+
+        theMenu = theMenu + '<ul>';
+
+        obj.forEach(function (item) {
+
+            theMenu = theMenu + '<li><a href="#">' + item.MenuName + '</a>';
+
+            if (item.Menus.length > 0) {
+
+                theMenu = theMenu + menuBuilder(item.Menus);
+
+            }
+
+            theMenu = theMenu + '</li>';
+
+        });
+
+        theMenu = theMenu + '</ul>';
+
+    } else {
+
+        console.log('no data');
+
+    }
+
+    return theMenu;
 }
