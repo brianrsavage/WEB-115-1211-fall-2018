aabfe94c0fb15064c1d712108fecfe1d7d91a8e5
Brian Savage <brian.richard.savage@gmail.com>
2018-11-01 09:50:04 -0400

    done 105

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,10 @@
-// JavaScript Document
+// inside out project STEP-105
 
 window.onload = init();
 
 function init() {
 
-    window.addEventListener('scroll', function (e) {
+    window.addEventListener('scroll', function(e) {
         var distanceY = window.pageYOffset || document.documentElement.scrollTop,
             shrinkOn = 300,
             header = document.querySelector("header");
@@ -17,40 +17,68 @@ function init() {
         }
     });
 
+
     $.ajax({
         method: 'GET',
         url: 'https://me.inside-out-project.com/wp-json/wp-api-menus/v2/menus/3',
         dataType: 'json',
-        success: function (data) {
+        success: function(data) {
             $('nav').hide();
-            $('#main').html('');
             var menu = menuBuilder(data.items);
             $('nav').html(menu).slideDown();
-           // $('nav').append(menu);
+            $('nav li a').click(function() {
+                getPage($(this).data("pgid"));
+            });
+            getPage(314);
             $("#loaderDiv").fadeOut("slow");
         },
-        error: function () {console.log('all is not good');
+        error: function() {
+            console.log('all is not good');
         }
     });
-
 }
 
 
 function menuBuilder(obj) {
-    let styCls = '';
     var theMenu = '';
     if (obj.length > 0) {
         theMenu = theMenu + '<ul>';
-        obj.forEach(function (item) {
-            theMenu = theMenu + '<li><a href="#">' + item.title + '</a>';
+        obj.forEach(function(item) {
+            theMenu = theMenu + '<li><a href="#" data-pgid="' + item.object_id + '">' + item.title + '</a>';
             if (item.children) {
                 theMenu = theMenu + menuBuilder(item.children);
             }
-            styCls = (styCls == '')?' class="color"':'';
-            $('#main').append('<section'+styCls+'><div class="container"><h1>' + item.title + '</h1></div></section>');
             theMenu = theMenu + '</li>';
         });
         theMenu = theMenu + '</ul>';
-    } else {console.log('no data');}
+    } else {
+        console.log('no data');
+    }
     return theMenu;
+}
+
+function getPage(obj) {
+    $("#loaderDiv").fadeIn("slow");
+    $.ajax({
+        method: 'GET',
+        url: 'https://me.inside-out-project.com/wp-json/wp/v2/pages/' + obj,
+        dataType: 'json',
+        success: function(data) {
+            var pgbuild = '';
+            pgbuild = '<section><div class="container">' + data.content.rendered + '</div></section>';
+            $("#content").fadeOut(function() {
+                $('html').animate({
+                    scrollTop: 0
+                }, 'slow'); //IE, FF
+                $('body').animate({
+                    scrollTop: 0
+                }, 'slow'); //chrome, don't know if Safari works
+                $(this).html(pgbuild).fadeIn();
+                $("#loaderDiv").fadeOut("slow");
+            });
+        },
+        error: function() {
+            console.log('bad');
+        }
+    });
 }
