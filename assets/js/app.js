367f73744c7e20e11c74440d55302ff7561f50a0
Brian Savage <brian.richard.savage@gmail.com>
2018-11-08 10:27:16 -0500

    explore ajax to function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,11 @@
-// inside out project STEP-105
+// inside out project STEP-106
 
 window.onload = init();
 
 function init() {
 
     window.addEventListener('scroll', function(e) {
+      console.log();
         var distanceY = window.pageYOffset || document.documentElement.scrollTop,
             shrinkOn = 300,
             header = document.querySelector("header");
@@ -18,41 +19,85 @@ function init() {
     });
 
 
+    apiCaller(3);
+    getPage(314);
+
     $.ajax({
         method: 'GET',
-        url: 'https://me.inside-out-project.com/wp-json/wp-api-menus/v2/menus/3',
+        url: 'https://me.inside-out-project.com/wp-json/wp-api-menus/v2/menus/12',
         dataType: 'json',
-        success: function(data) {
-            $('nav').hide();
-            var menu = menuBuilder(data.items);
-            $('nav').html(menu).slideDown();
-            $('nav li a').click(function() {
+        success: function (data) {
+            var menu = menuBuilder(data.items, 'genLinks', 'footer-ul');
+            $('#genLinks').replaceWith(menu);
+            $('#genLinks li a').click(function () {
                 getPage($(this).data("pgid"));
             });
-            getPage(314);
-            $("#loaderDiv").fadeOut("slow");
         },
-        error: function() {
-            console.log('all is not good');
+        error: function () {
+            errorHandler('We are having issues loading the General Links Menu.');
         }
     });
 }
 
+function apiCaller(pgId,currentMenu){
+  let curMenu = '';
+  curMenu = currentMenu;
 
-function menuBuilder(obj) {
+  $.ajax({
+      method: 'GET',
+      url: 'https://me.inside-out-project.com/wp-json/wp-api-menus/v2/menus/'+pgId,
+      dataType: 'json',
+      success: function(data) {
+          $('nav').hide();
+          var menu = menuBuilder(data.items);
+          if(curMenu == 'mainMenu'){
+            $('nav').html(menu).slideDown();
+            $('nav li a').click(function() {
+                getPage($(this).data("pgid"));
+            });
+          }
+          $("#loaderDiv").fadeOut("slow");
+      },
+      error: function() {
+          errorHandler('We are having issues loading the Main Menu.');
+      }
+  });
+
+
+}
+
+
+function errorHandler(erMsg){
+  console.log('all is not good');
+  let msg = '<div class="alert alert-danger" role="alert" style="text-align:center;">';
+  msg = msg+'There is curently a technical problem with the site.';
+  msg = msg+'<br>'+erMsg;
+  msg = msg+'<br>Please try again in a few minutes.';
+  msg = msg+'<br>If the problem continues please feel free to contact us at 555-555-5555.';
+  msg = msg+'</div>'
+  $('#main').prepend(msg).slideDown();
+  $("#loaderDiv").fadeOut("slow");
+}
+
+function menuBuilder(obj, targetEl, classInfo) {
+
     var theMenu = '';
     if (obj.length > 0) {
-        theMenu = theMenu + '<ul>';
-        obj.forEach(function(item) {
+      //console.log(targetEl);
+        let target = (targetEl)?' id="'+targetEl+'"':'';
+        let elClass = (classInfo)?' class="'+classInfo+'"':'';
+        theMenu = theMenu + '<ul' + target + ''+ elClass + '>';
+        //console.log(target);
+        obj.forEach(function (item) {
             theMenu = theMenu + '<li><a href="#" data-pgid="' + item.object_id + '">' + item.title + '</a>';
-            if (item.children) {
+           if (item.children) {
                 theMenu = theMenu + menuBuilder(item.children);
             }
             theMenu = theMenu + '</li>';
         });
         theMenu = theMenu + '</ul>';
     } else {
-        console.log('no data');
+        //console.log('no data');
     }
     return theMenu;
 }
@@ -78,7 +123,7 @@ function getPage(obj) {
             });
         },
         error: function() {
-            console.log('bad');
+            errorHandler('We are having issues loading the page.');
         }
     });
 }
